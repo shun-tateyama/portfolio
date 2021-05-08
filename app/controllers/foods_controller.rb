6068a95bf47d7ad77d6fbd6a9c7e28001283ac54
shun-tateyama <b1522783@planet.kanazawa-it.ac.jp>
2021-05-08 15:22:22 +0900

    ADD: controllerを追加

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,10 @@
+class FoodsController < ApplicationController
+  def index
+    restaurant = Restaurant.find(params[:restaurant_id])
+    foods = restaurant.foods
+
+    render json: {
+      foods: foods
+    }, status: :ok
+  end
+end
---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,9 @@
+class RestaurantsController < ApplicationController
+  def index
+    restaurants = Restaurant.all
+
+    render json: {
+      restaurants: restaurants
+    }, status: :ok
+  end
+end
